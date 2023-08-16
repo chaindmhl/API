3fd0bb9ca7eac757bc620d76414ee186f26e1d01
chaindmhl <117880660+chaindmhl@users.noreply.github.com>
2023-08-16 11:33:39 +0800

    Update process.py

--- process.py
+++ process.py
@@ -1,108 +1,104 @@
-import requests
 from django.conf import settings
-from json.decoder import JSONDecodeError
-from tracking.extract import (
-    frame_extract,
-)
-from tracking.models import Image
 import os
 import cv2
-from io import BytesIO
 from django.conf import settings
-from django.core.files.base import ContentFile
-from tracking.deepsort_tric.object_tracker import VehiclesCounting
-from tracking.models import Video, Frame
-from .serializers import VideoSerializers
+from django.core.files import File
+from tracking.deepsort_tric.object_tracker_cam_color import VehiclesCounting
+import datetime
+import requests
+from tracking.models import OutputVideo  # Replace 'myapp' with the name of your Django app
 
-import numpy as np
-import tensorflow as tf
 
 REQUEST_URL = f"http://{settings.HOST}:8000/"
 
 
-'''def process_vid(video):
-    # Frame Extraction
-    try:
-        response = requests.post(
-            REQUEST_URL + "tracking/extract/",
-            json={"video": video.id},
-        ).json()
-        image_ids = response["images"]
-    except JSONDecodeError:
-        image_ids = frame_extract(video)
-
-    # Tricycle Detection
-    frame_ids = tric_detect(video)
-
-    # TODO: Eye Detection
-    target_ids = []
-
-    return {
-        "video": video.id,
-        "images": image_ids,
-        "frames": frame_ids,
-        "targets": target_ids,
-    }'''
-def process_vid(video):
-   
+def process_vid(video_path=None, livestream_url=None, is_live_stream=False, video_stream=None):
     # Tricycle Detection and Tracking
-    # Load the video file
-    video_file = os.path.join(settings.MEDIA_ROOT, str(video.file))
-   
+    if video_path:
+        # Load the video file
+        video_file = video_path
 
         # Create a folder to store the output frames
-    output_folder_path = os.path.join(settings.MEDIA_ROOT, 'tracked_videos', str(video.id))
-    os.makedirs(output_folder_path, exist_ok=True)
+        output_folder_path = os.path.join(settings.MEDIA_ROOT, 'tracked_videos')
+        os.makedirs(output_folder_path, exist_ok=True)
 
-    # Specify the filename and format of the output video
-    output_video_path = os.path.join(output_folder_path, f"tracked_{video.id}.mp4")
-    
+        # Specify the filename and format of the output video
+        output_video_path = os.path.join(output_folder_path, f"tracked_{os.path.basename(video_path)}")
 
-    # Create an instance of the VehiclesCounting class
-    vc = VehiclesCounting(file_counter_log_name='vehicle_count.log',
-                          framework='tf',
-                          weights='/home/itwatcher/tricycle/tracking/deepsort_tric/checkpoints/yolov4-416',
-                          size=416,
-                          tiny=False,
-                          model='yolov4',
-                          video= video_file,
-                          output=output_video_path,
-                          output_format='XVID',
-                          iou=0.45,
-                          score=0.5,
-                          dont_show=False,
-                          info=False,
-                          detection_line=(0.5, 0))
+        # Create an instance of the VehiclesCounting class
+        vc = VehiclesCounting(file_counter_log_name='vehicle_count.log',
+                              framework='tf',
+                              weights='/home/itwatcher/API/tricycle_copy/tracking/deepsort_tric/checkpoints/yolov4-416',
+                              size=416,
+                              tiny=False,
+                              model='yolov4',
+                              video=video_file,
+                              output=output_video_path,
+                              output_format='XVID',
+                              iou=0.45,
+                              score=0.5,
+                              dont_show=False,
+                              info=False,
+                              detection_line=(0.5, 0))
 
-    vc.run()
+        # Run the tracking algorithm on the video
+        vc.run()
 
-    # Perform object tracking on the video frames
-    '''tracked_frames = vc.run(cap)
+        # Release the video capture object and close any open windows
+        #video_file.release()
+        cv2.destroyAllWindows()
 
-    # Save the frames with object tracking performed on them
-    frame_ids = []
-    for i, frame in enumerate(tracked_frames):
-        # Draw bounding boxes around the tracked objects
-        for obj in frame:
-            bbox = obj['bbox']
-            cv2.rectangle(frame, bbox[0:2], bbox[2:4], (0, 255, 0), 2)
-
-        # Save the frame as an image after object tracking has been performed on it
-        _, buffer = cv2.imencode(".jpg", frame)
-        content = ContentFile(BytesIO(buffer).getvalue())
-        frame_data = {"user": video.user, "video": video}
-        frame_instance = Frame.objects.create(**frame_data, file=content)
-        frame_instance.file.save(
-            f"tric{video.id}_{i}.jpg", content, save=True
+        # Create an instance of ObjectTrack and save the video file to it
+        object_track = OutputVideo(
+            # ... Other fields ...
+            video_file=File(open(output_video_path, 'rb'))
         )
-        print(f"Extracted frame id: {frame_instance.id}")
-        frame_ids.append(frame_instance.id)
+        object_track.save()
 
-    cap.release()
+        # Retrieve the object track from the database
+        object_track = OutputVideo.objects.get(id=object_track.id)
 
-    return {
-        "video": video.id,
-        "images": image_ids,
-        "frames": frame_ids,
-    }'''
+        # Get the URL of the saved video file
+        video_url = object_track.video_file.url
 
+        # Return the path to the output video and the URL of the saved video file
+        return {'output_video_path': output_video_path, 'video_url': video_url}
+
+    elif livestream_url:
+        # Check if the livestream_url is valid
+        response = requests.get(livestream_url, auth=('username', 'password'))
+        if response.status_code != 200:
+            # Handle invalid url    
+            return {'error': 'Invalid livestream url'}
+
+        # Create a VideoCapture object using the livestream url
+        video_file = cv2.VideoCapture(livestream_url)
+
+        # get the current timestamp
+        timestamp = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+
+        # create a unique output video name using the timestamp
+        output_video_path = os.path.join(output_folder_path, f'tracked_livestream_{timestamp}.avi')
+        os.makedirs(output_folder_path, exist_ok=True)
+
+        # Create an instance of the VehiclesCounting class
+        vc = VehiclesCounting(file_counter_log_name='vehicle_count.log',
+                              framework='tf',
+                              weights='/home/itwatcher/API/tricycle_copy/tracking/deepsort_tric/checkpoints/yolov4-416',
+                              size=416,
+                              tiny=False,
+                              model='yolov4',
+                              #video=stream_path,
+                              video=video_file,
+                              output=output_video_path,
+                              output_format='XVID',
+                              iou=0.45,
+                              score=0.5,
+                              dont_show=False,
+                              info=False,
+                              detection_line=(0.5, 0))
+
+        # Run the tracking algorithm on the video stream
+        vc.run()
+
+        # Release the video capture object and close any open windows
