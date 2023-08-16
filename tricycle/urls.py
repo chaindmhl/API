e65b074797c669a0f2f182bbdc40e798a93e09f3
chaindmhl <117880660+chaindmhl@users.noreply.github.com>
2023-08-16 11:36:47 +0800

    Update urls.py

--- tricycle/urls.py
+++ tricycle/urls.py
@@ -1,26 +1,21 @@
-"""
-URL configuration for tricycle project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-
+from rest_framework.routers import DefaultRouter
+from tracking.views import (
+    MyView,
+    PlateView,
+    FrameView,
+    MapView,
+)
+#original
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("tracking.urls")),
+    path('my-url/', MyView.as_view(), name='my-view'),
+    path('display_plates/', PlateView.as_view(), name='display_plates'),
+    path('view_frame/<int:log_id>/', FrameView.view_frame, name='view_frame'),
+    path('view_camera_map/<int:log_id>/', MapView.view_camera_map, name='view_camera_map'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
